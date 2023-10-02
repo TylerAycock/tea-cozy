e773a2424f3ed1b87906249bea4c06ba23ebdc05
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-10-02 13:45:23 -0700

    completed initial project

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Header from "./components/Header";
 import Mission from "./components/Mission";
 import TeaOfMonth from "./components/TeaOfMonth";
 import Locations from "./components/Locations";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="wrapper">
-      <Header/>
-      <Mission/>
-      <TeaOfMonth/>
-      <Locations/>
-      <footer>Footer</footer>
+      <Header />
+      <Mission />
+      <TeaOfMonth />
+      <Locations />
+      <Footer />
+      <p className="copyright">copyright The Tea Cozy 2017</p>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h2>The Tea Cozy</h2>
+      <h5>contact@theteacozy.com</h5>
+      <h5>917-555-8904</h5>
+    </div>
+  );
+};
+
+export default Footer
